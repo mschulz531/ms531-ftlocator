3270ecde6bf336cb356a3fd922b5ebe27fd7d33c
Michael Schulz <Michael.Schulz@spr.com>
2020-08-24 10:04:10 -0500

    changed function name

--- ftlocator.cs
+++ ftlocator.cs
@@ -10,9 +10,9 @@ using Newtonsoft.Json;
 
 namespace mschulz.ftlocator
 {
-    public static class ft_locator_func
+    public static class ftlocator
     {
-        [FunctionName("ft_locator_func")]
+        [FunctionName("ftlocator")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
